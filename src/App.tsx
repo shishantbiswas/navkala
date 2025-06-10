8401c9aab31abe992616ab572113898f5fcd56d1
installer <installer@gmail.com>
2025-06-10 16:25:09 +0530

    2

--- src/App.tsx
+++ src/App.tsx
@@ -14,78 +14,9 @@ export default function App() {
           />
         </div>
         <div className=" flex flex-col items-center justify-center">
-          <div className="  bg-black/50 p-8  md:my-8 rounded-xl  relative h-screen overflow-hidden md:h-fit">
-            {/* <div className="flex items-center justify-center">
-              <img
-                src="/icon.webp"
-                className="absolute top-0 right-0 h-full w-full object-cover blur-2xl opacity-30"
-              />
-            </div> */}
-
-            {/* <div className="mt-12 text-white flex flex-col gap-6">
-              <h1 className=" uppercase text-center text-3xl">
-                Donation and contribution
-              </h1>
-              <div className=" flex gap-4 justify-center">
-                <a
-                  href="https://paypal.me/stynbsws"
-                  target="_blank"
-                  className="py-2 flex px-4 gap-2 items-center bg-white/10 hover:bg-[#003087] text-xl rounded-md text-nowrap capitalize text-white font-medium w-fit"
-                >
-                  <FaPaypal size={20} />
-                  <p>PayPal</p>
-                </a>
-                <a
-                  href="https://paypal.me/stynbsws"
-                  target="_blank"
-                  className="py-2 flex px-4 gap-2 items-center hover:bg-[#FFDD00] hover:text-black bg-white/10 text-xl rounded-md text-nowrap capitalize text-white font-medium w-fit"
-                >
-                  <SiBuymeacoffee size={20} />
-                  <p>BuyMeACoffee</p>
-                </a>
-                <a
-                  href="https://paypal.me/stynbsws"
-                  target="_blank"
-                  className="py-2 flex px-4 gap-2 items-center hover:bg-[#FF5E5B] bg-white/10 text-xl rounded-md text-nowrap capitalize text-white font-medium w-fit"
-                >
-                  <SiKofi size={20} />
-                  <p>Kofi</p>
-                </a>
-                <a
-                  href="https://paypal.me/stynbsws"
-                  target="_blank"
-                  className="py-2 flex px-4 gap-2 items-center hover:bg-[#20336B] bg-white/10 text-xl rounded-md text-nowrap capitalize text-white font-medium w-fit"
-                >
-                  <SiPaytm size={20} />
-                  <p>Paytm</p>
-                </a>
-              </div>
-            </div> */}
-            {/* <div className="mt-12 text-white flex flex-col gap-6 ">
-              <h1 className=" uppercase text-center text-3xl">
-                Connet to community
-              </h1>
-              <div className=" flex gap-4 justify-center">
-                <a
-                  href="https://paypal.me/stynbsws"
-                  target="_blank"
-                  className="py-2 flex px-4 gap-2 hover:bg-[#5865F2] items-center bg-white/10 text-xl rounded-md text-nowrap capitalize text-white font-medium w-fit"
-                >
-                  <FaDiscord size={20} />
-                  <p>Discord</p>
-                </a>
-                <a
-                  href="https://paypal.me/stynbsws"
-                  target="_blank"
-                  className="py-2 flex px-4 gap-2  hover:bg-[#FF4500]  items-center bg-white/10 text-xl rounded-md text-nowrap capitalize text-white font-medium w-fit"
-                >
-                  <FaReddit size={20} />
-                  <p>Reddit</p>
-                </a>
-              </div>
-            </div> */}
+          <div className="  bg-black/50 p-2 min-[500px]:p-4  min-[500px]:my-8 rounded-xl  relative min-h-screen md:h-fit">
             <Navbar />
-            <Seperator className="mt-12 mb-8 md:mb-12"/>
+            <Seperator className="my-5 md:mb-12 h-0.5 w-[85%]" />
             <Products />
           </div>
         </div>
@@ -97,27 +28,21 @@ export default function App() {
 function Seperator({ className }: { className?: string }) {
   return (
     <div
-      className={`h-1  w-[90%] relative justify-self-center rounded-full bg-white/30 z-50 ${
-        className ?? ""
-      }`}
+      className={`bg-white/30 relative justify-self-center rounded-full z-50 ${className ?? ""
+        }`}
     ></div>
   );
 }
 
 function Products() {
   return (
-    <div>
-      <h1 className="uppercase text-center text-3xl">Products</h1>
-      <div className="grid md:px-6 md:gap-4 min-[500px]:grid-cols-2 ">
-        {/* <Product src="/kapda.webp" title="Vanity Fair Italy" />
-        <Product src="/nela-kapda.webp" title="Vanity Fair Italy" /> */}
-        <Product src="/chakri.webp"  />
-        <Product src="/9.webp"  />
-        <Product src="/16.webp"  />
-        <Product src="/1.webp"  />
-      
-        {/* <Product src="/9.webp" title="Vanity Fair Italy" />
-        <Product src="/9.webp" title="Vanity Fair Italy" /> */}
+    <div className="">
+      <h1 className="uppercase text-center">Products</h1>
+      <div className="grid md:px-6 md:gap-4 grid-cols-2 ">
+        <Product src="/chakri.webp" />
+        <Product src="/9.webp" />
+        <Product src="/16.webp" />
+        <Product src="/1.webp" />
       </div>
     </div>
   );
@@ -125,10 +50,10 @@ function Products() {
 
 function Product({ src, title }: { title?: string; src: string }) {
   return (
-    <div className="flex flex-col p-12  md:aspect-auto  rounded-2xl items-center gap-8  hover:bg-white/20">
+    <div className="flex flex-col p-4 md:aspect-auto  rounded-2xl items-center gap-8 ">
       <img
         src={src}
-        className="md:size-[140px] aspect-square  rounded-2xl"
+        className="min-[500px]:size-[120px] sm:size-[150px] aspect-square  rounded-2xl"
         alt={title}
       />
       {title && <h3 className="text-3xl md:text-xl">{title}</h3>}
@@ -138,31 +63,36 @@ function Product({ src, title }: { title?: string; src: string }) {
 
 function Navbar() {
   return (
-    <div className="flex items-center px-8 justify-between">
+    <div className="my-4 flex items-center-safe px-8 justify-between">
       <div>
         <img
-          className="size-[100px] aspect-square object-cover rounded-full  !duration-0 !transition-none"
+          className="size-[140px] aspect-square object-cover rounded-full  !duration-0 !transition-none"
           src={"/icon.webp"}
         />
       </div>
-      <div className="mt-4 flex flex-col  gap-4 items-center justify-center">
-        <a
-          href="https://wa.me/c/917701871656"
-          target="_blank"
-          className="py-2 w-full flex px-4 gap-2 bg-[#25D366] items-center justify-center hover:text-white text-xl rounded-full text-nowrap capitalize text-white font-medium md:w-fit z-50 scale-90"
-        >
-          <FaWhatsapp size={25} />
-          <p>WhatsApp</p>
-        </a>
-        <a
-          href="https://www.instagram.com/navkala_bsws/"
-          target="_blank"
-          className="py-2 w-full flex px-4 gap-2 bg-gradient-to-tr from-[#f09433] via-[#dc2743] to-[#bc1888] text-white p-4  items-center justify-center hover:text-white text-xl rounded-full text-nowrap capitalize  font-medium md:w-fit  z-50 scale-90"
-        >
-          <FaInstagram size={25} />
-          <p>Instagram</p>
-        </a>
+      <div className="flex items-center justify-center flex-col">
+        <h1 className="uppercase text-center">SHOP NOW</h1>
+        <Seperator className="h-0.5 mt-0.5  w-[70%]"/>
+        <div className="flex min-[500px]:flex-col mt-2 gap-2 items-center justify-center">
+          <a
+            href="https://wa.me/c/917701871656"
+            target="_blank"
+            className="py-2 w-full flex px-4 gap-2 bg-[#25D366] items-center justify-center hover:text-white text-sm rounded-full text-nowrap capitalize text-white font-medium md:w-fit z-50 scale-80 max-[500px]:aspect-square"
+          >
+            <FaWhatsapp size={25} />
+            <p className="min-[500px]:block hidden ">WhatsApp</p>
+          </a>
+          <a
+            href="https://www.instagram.com/navkala_bsws/"
+            target="_blank"
+            className="py-2 w-full flex px-4 gap-2 bg-gradient-to-tr from-[#f09433] via-[#dc2743] to-[#bc1888] text-white p-4  items-center justify-center hover:text-white text-sm rounded-full text-nowrap capitalize font-medium md:w-fit  z-50 scale-80 max-[500px]:aspect-square"
+          >
+            <FaInstagram size={25} />
+            <p className="min-[500px]:block hidden">Instagram</p>
+          </a>
+        </div>
       </div>
+
     </div>
   );
 }
